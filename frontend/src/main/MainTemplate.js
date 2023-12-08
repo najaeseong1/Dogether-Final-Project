3d35ef780ca3e29d02eb346f350fb3fce5806054
jihoo2002 <yolohoo@naver.com>
2023-12-08 15:25:50 +0900

    ADD 헤더푸터 작업중

---
+++ frontend/src/main/MainTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainTemplate.scss';
+const MainTemplate = () => {
+  return (
+    <>
+      <div>
+        <div>MainTemplate</div>
+      </div>
+    </>
+  );
+};
+
+export default MainTemplate;
